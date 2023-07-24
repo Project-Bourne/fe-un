4eabc0ead5a2ab7e3680d4180cab2da87036c045
Babangida Tsowa <deepone@Babangidas-MacBook-Pro-2.local>
2023-07-24 13:51:25 +0100

    UPDATED: /documents UI

--- src/pages/documents/components/docCards.tsx
+++ src/pages/documents/components/docCards.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 function DocCard() {
   return (
-    <div className="w-full rounded-xl bg-[#F9F9F9] hover:bg-[#D1F1FA] hover:border-[#4AC7ED] my-2 p-5 border-2 border-grey-300">
+    <div className="w-full cursor-pointer rounded-xl bg-[#F9F9F9] hover:bg-[#D1F1FA] hover:border-[#4AC7ED] my-2 p-5 border-2 border-grey-300">
       <div className="flex items-center align-middle justify-between">
         <span className="bg-white rounded-2xl border py-1 px-2 border-[#E8EAEC] text-[grey]">
           Crawler
