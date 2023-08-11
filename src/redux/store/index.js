03acc4e050323b0ff951557b2b0e725eb38c1da2
chisom chima <chimachisom360@gmail.com>
2023-08-11 10:58:05 +0100

    merge fixes

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,32 @@
 // store/index.js
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from '../reducers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "../reducers";
 
-
-const store = configureStore({
-  reducer: rootReducer,
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
