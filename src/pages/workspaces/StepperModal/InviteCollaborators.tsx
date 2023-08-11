bfea6328332194b9c863bcda375b68439f213de7
Legacy <legacy.rexio@gmail.com>
2023-08-11 12:49:02 +0100

    build successful

--- src/pages/workspaces/StepperModal/InviteCollaborators.tsx
+++ src/pages/workspaces/StepperModal/InviteCollaborators.tsx
@@ -10,9 +10,11 @@ import { toast } from "react-toastify";
 import { setCollab, setSpaceId } from "@/redux/reducers/workspaceReducer";
 
 const InviteCollaborators = (props) => {
-  const [isDisabled, setIsDisabled] = useState(true)
+  const [isDisabled, setIsDisabled] = useState(true);
   const { stages, index, setIndex } = props;
-  let createspace = useSelector((state) => state?.workSpace?.createSpace);
+  const createspace = useSelector(
+    (state: any) => state?.workSpace?.createSpace,
+  );
   const userService = new globalService();
   const workspaceService = new CollabService();
   const dispatch = useDispatch();
@@ -40,13 +42,13 @@ const InviteCollaborators = (props) => {
       }
     });
     if (selectedSuggestions.length < 1) {
-      setIsDisabled(false)
+      setIsDisabled(false);
     }
     console.log(selectedSuggestions);
   };
   const goBack = () => {
-    setIndex(index - 1)
-  }
+    setIndex(index - 1);
+  };
   // Handle the invite button click
   const handleInvite = async () => {
     dispatch(setCollab(selectedSuggestions));
