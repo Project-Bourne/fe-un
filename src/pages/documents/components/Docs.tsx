89c7ccc72609c993507585af565df95735d237b9
Babangida Tsowa <tsowababangida@gmail.com>
2024-07-23 17:38:24 +0100

    Increased Width of Comments for Better Fit in UI

--- src/pages/documents/components/Docs.tsx
+++ src/pages/documents/components/Docs.tsx
@@ -9,11 +9,17 @@ export default function Docs({ showComments, setShowComments }) {
   const router = useRouter();
   return (
     <div className="w-full h-[100vh] overflow-y-hidden flex items-center justify-center">
-      <div className="w-[80%] h-full shadow overflow-y-auto my-10 pb-[300px]">
+      <div
+        className={
+          showComments
+            ? "w-[65%]"
+            : "w-[80%]" + " h-full shadow overflow-y-auto my-10 pb-[300px]"
+        }
+      >
         <TextEditor />
       </div>
       {showComments && (
-        <div className="w-[20%] h-full ">
+        <div className="w-[33%] h-full ">
           <Chats setShowChat={setShowComments} />
         </div>
       )}
